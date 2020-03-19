69852ce4d14f0e2c06e2adb4def2dcdb30a4f542
Jennifer Miller <jmillerarcher@me.com>
2020-03-19 20:14:36 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors, only: [:show] do
-    resources :posts do
-    resources :comments
-    end
+    
     # nested resource for posts
     resources :posts, only: [:show, :index]
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   
-  resources :posts do
-  resources :comments
-  end
+  
 
   root 'posts#index'
 
